7b986825a420695ee0473062b36eed2665f9c5ce
Pratik Rout <routpratik2001@gmail.com>
2022-02-23 13:09:52 +0530

    practice program

--- main.c
+++ main.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
-#include <stdarg.h>
-#include <math.h>
 
-double sample_stddev(int count, ...);
+char *reverse(char *str);
 
 int main(void){
-    printf("Standard dev = %f\n", sample_stddev(4, 30.0, 27.3, 26.9, 25.7));
+    char str[100];
+    char *rev = NULL;
+
+    printf("Enter the string: ");
+    scanf("%s", str);
+
+    printf("The original string is: %s\n", str);
+    
+    rev = reverse(str);
+
+    printf("The reversed string is: %s\n", rev);
 
     return 0;
 }
 
-double sample_stddev(int count, ...){
-    double sum = 0;
+char *reverse(char *str){
+    static int i = 0;
+    static char rev[100];
 
-    va_list args1;
-    va_start(args1, count);
-
-    va_list args2;
-    va_copy(args2, args1);
-    
-    for(int i = 0; i < count; i++){
-        double num = va_arg(args1, double);
-        sum += num;
+    if(*str){
+        reverse(str + 1);
+        rev[i++] = *str;
     }
 
-    va_end(args1);
-
-    double mean = sum/count;
-    double sum_sq_diff = 0;
-
-    for(int i = 0; i < count; i++){
-        double num = va_arg(args2, double);
-        sum_sq_diff += (num - mean) * (num - mean);
-    }
-
-    va_end(args2);
-    return sqrt(sum_sq_diff/count);
+    return rev;
 }
